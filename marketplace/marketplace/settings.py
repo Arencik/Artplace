8b19b837bd2da8a32f604a150735ef607dc4fe38
Arencik <80043391+Arencik@users.noreply.github.com>
2023-05-11 17:18:17 +0200

    Update settings.py

--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ll4w_gm^xm#t2#sm&b!^mlrc_&c6s+naphm2o06($5zm_domx&'
+SECRET_KEY = #
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
